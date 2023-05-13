eda7109a29987f94fb34bae11c2e878a3b5d8103
Miguel Puentes <mipuentesc@unal.edu.co>
2023-05-13 15:27:34 -0500

    Fixed small bug in chat

--- tutoacademy_wa/src/components/chat/profiles.jsx
+++ tutoacademy_wa/src/components/chat/profiles.jsx
@@ -3,20 +3,34 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
+import { useAuthUser } from 'react-auth-kit';
 
 export function ProfilesChat(props) {
+  const authUser=useAuthUser();
+  const user=authUser();
   const handleClick = () => {
     if (props.onClick) {
       props.onClick(props.data);
     }
   };
-
-  return (
-    <ListItem button key={props.data.receiver.userID.googleId} onClick={handleClick}>
-      <ListItemIcon>
-        <Avatar alt={props.data.receiver.fullname} src={props.data.receiver.userID.imageUrl} />
-      </ListItemIcon>
-      <ListItemText primary={props.data.receiver.fullname}>{props.data.receiver.fullname}</ListItemText>
-    </ListItem>
-  );
+  if (user.googleId == props.data.receiver.userID.googleId){
+    return (
+        <ListItem button key={props.data.sender.userID.googleId} onClick={handleClick}>
+        <ListItemIcon>
+            <Avatar alt={props.data.sender.fullname} src={props.data.sender.userID.imageUrl} />
+        </ListItemIcon>
+        <ListItemText primary={props.data.sender.fullname}>{props.data.sender.fullname}</ListItemText>
+        </ListItem>
+    );
+  }
+  else {
+    return (
+            <ListItem button key={props.data.receiver.userID.googleId} onClick={handleClick}>
+            <ListItemIcon>
+                <Avatar alt={props.data.receiver.fullname} src={props.data.receiver.userID.imageUrl} />
+            </ListItemIcon>
+            <ListItemText primary={props.data.receiver.fullname}>{props.data.receiver.fullname}</ListItemText>
+            </ListItem>
+        );
+  }
 }
