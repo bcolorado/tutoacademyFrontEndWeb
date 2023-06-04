aea113e9bd3ffa74d13aec375272676192ec89df
bcolorado <bcolorado@unal.edu.co>
2023-06-04 16:41:14 -0500

    AG URL changed

--- tutoacademy_wa/src/components/home/profileResult.jsx
+++ tutoacademy_wa/src/components/home/profileResult.jsx
@@ -1,17 +1,17 @@
-import { Typography, Stack } from '@mui/material'
-import React from 'react'
+import { Typography, Stack } from '@mui/material';
+import React from 'react';
 import { Link as RouterLink } from "react-router-dom";
 import { Avatar } from '@mui/material';
 
 export function ProfileResult(props) {
-    return (
-        <RouterLink to={`/profile/${props.data.userID.googleId}`} style={{ textDecoration: "none", color: "black" }} component={React.Fragment}>
-        <Stack direction="row" spacing={2} alignItems="center" >
-            <Avatar sx={{ width: "35px", height: "35px" }} src={props.data.userID.imageUrl} alt={`${props.data.fullname}'s profile picture`} />
-            <Typography noWrap pl="10px" maxWidth={300}>
-            {props.data?.fullname}
-            </Typography>
-        </Stack>
-        </RouterLink>
-    )
+  return (
+    <RouterLink to={`/profile/${props.data.userID.googleId}`} style={{ textDecoration: "none", color: "black" }}>
+      <Stack direction="row" spacing={2} alignItems="center">
+        <Avatar sx={{ width: "35px", height: "35px" }} src={props.data.userID.imageUrl} alt={`${props.data.fullname}'s profile picture`} />
+        <Typography noWrap pl="10px" maxWidth={300}>
+          {props.data?.fullname}
+        </Typography>
+      </Stack>
+    </RouterLink>
+  );
 }
